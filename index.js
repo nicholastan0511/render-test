f9cd67a93f417357de3b812c3985ac50d509788a
Nicholas Tan <nicholastan0511@gmail.com>
2023-12-09 20:54:49 +0800

    commit frontend

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ let notes = [
 ]
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -78,7 +79,7 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
